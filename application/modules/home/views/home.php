7eefe3cb953f6d61f4483d52357eec45376b3113
Rifqi Hakim <rifqihakim5889@gmail.com>
2022-10-27 14:16:18 +0700

    -

--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -2,9 +2,9 @@
 	<h1 class="cover-heading">SIAKAD <?= $infosekolah['sekolah'] ?></h1>
 	<p class="lead">Sistem Informasi Akademik</p>
 	<p class="lead">
-		<a href="<?= base_url('loginppdb') ?>" class="btn btn-lg btn-secondary">PPDB</a>
-		<a href="<?= base_url('loginsiswa') ?>" class="btn btn-lg btn-secondary">Siswa</a>
-		<a href="<?= base_url('loginguru') ?>" class="btn btn-lg btn-secondary">Guru</a>
-		<a href="<?= base_url('login') ?>" class="btn btn-lg btn-secondary">Admin</a>
+		<a style="margin-top: 20px;" href="<?= base_url('loginppdb') ?>" class="btn btn-lg btn-secondary">PPDB</a>
+		<a style="margin-top: 20px;" href="<?= base_url('loginsiswa') ?>" class="btn btn-lg btn-secondary">Siswa</a>
+		<a style="margin-top: 20px;" href="<?= base_url('loginguru') ?>" class="btn btn-lg btn-secondary">Guru</a>
+		<a style="margin-top: 20px;" href="<?= base_url('login') ?>" class="btn btn-lg btn-secondary">Admin</a>
 	</p>
 </main>
--- application/views/themes/guru/auth/header.php
+++ application/views/themes/guru/auth/header.php
@@ -5,20 +5,23 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?= $title; ?></title>
+
   <link rel="shortcut icon" href="<?= base_url('assets/favicon.ico')?>">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/bower_components/font-awesome/css/font-awesome.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/bower_components/font-awesome/css/font-awesome.min.css') ?>">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/bower_components/Ionicons/css/ionicons.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/bower_components/Ionicons/css/ionicons.min.css') ?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/dist/css/AdminLTE.min.css'); ?>">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/plugins/iCheck/square/blue.css'); ?>">
-
+  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/dist/css/AdminLTE.min.css') ?>">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/dist/css/skins/_all-skins.min.css') ?>">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?= base_url('assets/themes/backend/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') ?>">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -28,6 +31,23 @@
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  <!-- CKE -->
+  <script src="<?= base_url('assets/themes/backend/plugins/ckeditor/ckeditor.js') ?>"></script>
+
+
+    <style>
+
+    body {
+    
+        background: url("<?= base_url('assets/themes/frontend/images/bg.jpg')?>") no-repeat center center fixed; 
+        background-size: cover;
+    }
+    </style>
+    <!-- Custom styles for this template -->
+    <!-- <link href="<?= base_url('assets/themes/frontend/css/cover.css') ?>" rel="stylesheet"> -->
 </head>
 
-<body class="hold-transition <?= $body_class; ?>">
+<body class="hold-transition sidebar-mini">
+  <!-- Site wrapper -->
+  <div class="wrapper">
