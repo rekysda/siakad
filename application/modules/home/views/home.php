6972a649da16da1ba6b58b2c92cd3cc3879f0cf8
Programmer_Pemula <rekysda@gmail.com>
2022-10-27 14:12:51 +0700

    Update home.php

--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -2,9 +2,9 @@
 	<h1 class="cover-heading">SIAKAD <?= $infosekolah['sekolah'] ?></h1>
 	<p class="lead">Sistem Informasi Akademik</p>
 	<p class="lead">
-		<a href="<?= base_url('loginppdb') ?>" class="btn btn-lg btn-secondary">Login PPDB</a>
-		<a href="<?= base_url('loginsiswa') ?>" class="btn btn-lg btn-secondary">Login Siswa</a>
-		<a href="<?= base_url('loginguru') ?>" class="btn btn-lg btn-secondary">Login Guru</a>
-		<a href="<?= base_url('login') ?>" class="btn btn-lg btn-secondary">Login Admin</a>
+		<a href="<?= base_url('loginppdb') ?>" class="btn btn-lg btn-secondary">PPDB</a>
+		<a href="<?= base_url('loginsiswa') ?>" class="btn btn-lg btn-secondary">Siswa</a>
+		<a href="<?= base_url('loginguru') ?>" class="btn btn-lg btn-secondary">Guru</a>
+		<a href="<?= base_url('login') ?>" class="btn btn-lg btn-secondary">Admin</a>
 	</p>
 </main>
